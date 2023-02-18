6070f39366aad746666e3f27235dbca9eff4947b
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-02-18 10:40:25 +0900

    chore: replace infra with sam tmpl

---
+++ scraping/functions/put_queue_fn/app.py
@@ -0,0 +1,76 @@
+import decimal
+import json
+import os
+from time import sleep
+
+import boto3
+from botocore.exceptions import ClientError
+
+# dynamo
+table_name = os.environ.get("DYNAMODB_SCRAPING_TABLE", "Scrapings")
+region = os.environ.get("REGION_NAME", "ap-northeast-1")
+
+scraping_table = boto3.resource("dynamodb", region_name=region).Table(table_name)
+
+
+# SQS
+SQS = boto3.resource("sqs")
+QUEUE = SQS.Queue("url")
+URL = os.environ["QUEUE_URL"]
+
+
+class DecimalEncoder(json.JSONEncoder):
+    """dynamodb get_item return number type for encoder."""
+
+    def default(self, obj):
+        """Return encode data."""
+        print("Return encode data.")
+        if isinstance(obj, decimal.Decimal):
+            if obj % 1 > 0:
+                return float(obj)
+            else:
+                return int(obj)
+        return super(DecimalEncoder, self).default(obj)
+
+
+def get_item(key):
+    """DynamoDBからitemを取得する."""
+    print("DynamoDBからitemを取得する.")
+    try:
+        response = scraping_table.get_item(Key={"ContentsName": str(key)})
+        encode = json.dumps(response["Item"], cls=DecimalEncoder, ensure_ascii=False)
+        return encode
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+        raise e
+
+
+def put_sqs(item):
+    """sqsのキューにメッセージをputする."""
+    print("sqsのキューにメッセージをput")
+    print(item)
+    QUEUE.send_message(MessageBody=item, QueueUrl=URL, DelaySeconds=0)
+
+
+def key_list(filename):
+    """コピー対象のuseridをテキストから取得."""
+    print("コピー対象のuseridをテキストから取得.")
+    path = os.getcwd() + f"/list/{filename}"
+    with open(path) as f:
+        # 一行ごとをリストに入れてreturn
+        return f.read().split()
+
+
+def lambda_handler(event, context):
+    """main."""
+    try:
+        filelist = ["get_key_list.txt"]
+        for f in filelist:
+            # for i in key_list(f):
+            for i in ["whoscored", "fabrizioromano", "sofascoreint", "shotaro59432703"]:
+                result = get_item(i)
+                put_sqs(result)
+                # 1秒スリープ処理
+                sleep(1)
+    except Exception as e:
+        raise e
---
+++ scraping/functions/sync_fn/app.py
@@ -0,0 +1,79 @@
+import ast
+import json
+import os
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+# dynamo
+table_name = os.environ.get("DYNAMODB_ARTICLE_TABLE", "Articles")
+region = os.environ.get("REGION_NAME", "ap-northeast-1")
+
+article_table = boto3.resource("dynamodb", region_name=region).Table(table_name)
+
+
+def clean_table(UserID):
+    """コピー先のDynamoDBテーブルをcleanUpする."""
+    print("コピー先のDynamoDBテーブルをcleanUpする.")
+    getCleanTable = article_table.query(KeyConditionExpression=Key("UserID").eq(UserID))
+    print(getCleanTable)
+
+    for i in getCleanTable["Items"]:
+        print(i)
+        cleanupTable = boto3.client("dynamodb").batch_write_item(
+            RequestItems={
+                "Articles": [
+                    {
+                        "DeleteRequest": {
+                            "Key": {
+                                "UserID": {"S": i["UserID"]},
+                                "ArticleID": {"S": i["ArticleID"]},
+                            }
+                        }
+                    }
+                ]
+            }
+        )
+
+
+def put_dynamo(data):
+    """コピー先のDynamoDBテーブルへputする."""
+    print("コピー先のDynamoDBテーブルへputする.")
+    UserName = data["ContentsName"]
+    contents_list = json.loads(data["Contents"])
+    UserID = str(contents_list[0]["authorId"])
+    AccountName = data["URL"]
+    clean_table(UserID)
+    try:
+        for i in range(len(contents_list)):
+            Content = {
+                "ArticleID": str(contents_list[i]["tweetId"]),
+                "UserID": str(contents_list[i]["authorId"]),
+                "AccountName": AccountName,
+                "Detail": repr(contents_list[i]["body"]),
+                "UserName": UserName,
+                "ImgPath": str(contents_list[i]["imgPath"]),
+                "CreatedAt": contents_list[i]["createdAt"],
+            }
+            article_table.put_item(Item=Content)
+
+    except Exception as error:
+        raise error
+
+
+def conv_dict(strings):
+    """Str -> Dict."""
+    print("Str -> Dict.")
+    dict = ast.literal_eval(str(strings))
+    return dict
+
+
+def lambda_handler(event, context):
+    """main."""
+    print("main.")
+    try:
+        for i in range(len(event["Records"])):
+            getdata = event["Records"][i - 1]["body"]
+            put_dynamo(conv_dict(getdata))
+    except Exception as error:
+        raise error
