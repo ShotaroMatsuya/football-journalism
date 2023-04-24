fd43f77e66723b99ec7b8b7c498bc77880eed972
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-04-24 20:28:58 +0900

    chore: setup cypress environments

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,13 @@ module.exports = defineConfig({
   screenshotsFolder: './cypress/snapshots/actual',
   trashAssetsBeforeRuns: true,
   video: false,
-
+  experimentalWebKitSupport: true,
   env: {
     failSilently: false,
+    SNAPSHOT_BASE_DIRECTORY: 'cypress/snapshots/base',
+    SNAPSHOT_DIFF_DIRECTORY: 'cypress/snapshots/diff',
     ALWAYS_GENERATE_DIFF: false,
-    ALLOW_VISUAL_REGRESSION_TO_FAIL: true,
+    type: 'actual',
   },
 
   e2e: {
@@ -17,5 +19,13 @@ module.exports = defineConfig({
       // implement node event listeners here
       getCompareSnapshotsPlugin(on, config);
     },
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  retries: {
+    runMode: 2,
+    openMode: 0,
   },
 });
