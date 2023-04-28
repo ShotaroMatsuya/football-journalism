92de50f2febe306a01478e1e18784ec30fea54ab
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-04-29 03:37:32 +0900

    chore: modify configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,30 @@
 module.exports = {
   root: true,
-
   env: {
-    node: true,
+    node: true
   },
-
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'prettier',
+    '@vue/typescript/recommended',
+    'prettier'
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
-
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
-        jest: true,
-      },
-    },
-  ],
-};
+        jest: true
+      }
+    }
+  ]
+}
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  configureWebpack: {
-    devtool: 'source-map',
-  },
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false
+})
